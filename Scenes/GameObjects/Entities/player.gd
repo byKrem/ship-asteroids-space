35da23f3295270a95d014134d5efc773d49ee9f6
Rubanov <bykr3m@gmail.com>
2025-08-31 18:59:37 +0300

    Player life counter

--- Scenes/GameObjects/Entities/player.gd
+++ Scenes/GameObjects/Entities/player.gd
@@ -2,24 +2,31 @@ extends CharacterBody2D
 
 const SPEED = 10.0
 
+signal ship_wrapped()
 @export var BULLET : PackedScene
+@onready var life_counter: HBoxContainer = $"../CanvasLayer/LifeCounter"
 
 var viewport_rect: Rect2
 var half_sprite_width: float
 var half_sprite_height: float
 var fire_in_cooldown: bool = false
+var extra_life_count: int = 3
 @onready var angle: float = ProjectSettings.get_setting("gameplay/angle_sensitivity")
 
 func wrap_pos() -> void:
 	if global_position.x < viewport_rect.position.x - half_sprite_width:
 		global_position.x = viewport_rect.end.x + half_sprite_width
+		ship_wrapped.emit()
 	elif global_position.x > viewport_rect.end.x + half_sprite_width:
 		global_position.x = viewport_rect.position.x - half_sprite_width
+		ship_wrapped.emit()
 		
 	if global_position.y < viewport_rect.position.y - half_sprite_height:
 		global_position.y = viewport_rect.end.y + half_sprite_height
+		ship_wrapped.emit()
 	elif global_position.y > viewport_rect.end.y + half_sprite_height:
 		global_position.y = viewport_rect.position.y - half_sprite_height
+		ship_wrapped.emit()
 
 func fire() -> void:
 	var bullet = BULLET.instantiate()
@@ -37,7 +44,6 @@ func _ready() -> void:
 	half_sprite_height = $Sprite2D.texture.get_height() * $Sprite2D.scale.y / 6
 	
 	ProjectSettings.settings_changed.connect(_on_settings_changed)
-	
 
 func _on_settings_changed() -> void:
 	angle = ProjectSettings.get_setting("gameplay/angle_sensitivity")
@@ -70,4 +76,24 @@ func _on_shoot_cooldown_timeout() -> void:
 
 
 func _on_health_component_on_health_run_out() -> void:
-	self.call_deferred("queue_free")
+	extra_life_count -= 1
+	var life_to_remove = life_counter.get_child(0)
+	life_counter.remove_child(life_to_remove)
+	
+	if extra_life_count <= 0:
+		self.call_deferred("queue_free")
+	else:
+		$HealthComponent.health = 1
+		immune_toggle()
+		$ImmuneTimer.start()
+
+func immune_toggle() -> void:
+	$HurtboxComponent.set_deferred("monitorable", !$HurtboxComponent.monitorable)
+	$HurtboxComponent.set_deferred("monitoring", !$HurtboxComponent.monitoring)
+	if $AnimationPlayer.is_playing():
+		$AnimationPlayer.stop()
+	else:
+		$AnimationPlayer.play("immuneAnim")
+
+func _on_immune_timer_timeout() -> void:
+	immune_toggle()
