75b6295aa9136a68821d9e1c20816521209961d1
Rubanov <bykr3m@gmail.com>
2025-08-31 22:50:41 +0300

    Changed main menu
    Just made it pretty

--- Scenes/UI/volume_slider.gd
+++ Scenes/UI/volume_slider.gd
@@ -7,8 +7,8 @@ extends Container
 func _ready() -> void:
 	$ValueSlider.value = AudioServer.get_bus_volume_linear(_bus_index)
 	$Name.text = bus_name
-	$Value.text = str(AudioServer.get_bus_volume_linear(_bus_index) * 100) + "%"
+	$Value.text = str(int(round(AudioServer.get_bus_volume_linear(_bus_index) * 100))) + "%"
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(_bus_index, value)
-	$Value.text = str(value*100) + "%"
+	$Value.text = str(int(round(value*100))) + "%"
